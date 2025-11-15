024ba59d310d234c5267d34a55192b2339413901
Jehojada <saccajeho@gmail.com>
2025-11-15 23:07:03 +0100

    Allow admin sidebar scrolling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -118,7 +118,7 @@ export default function Navigation({ userType = 'player', currentPage = '', onPa
         </div>
 
         {/* Navigation Items */}
-        <nav className="flex-1 p-4 overflow-y-auto">
+        <nav className="flex-1 min-h-0 p-4 overflow-y-auto">
           <ul className="space-y-2">
             {navItems.map((item) => {
               const Icon = item.icon;
